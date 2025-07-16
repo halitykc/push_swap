5986062362fb0a26ad4aa5fc749a8f5a028bf97e
Halit Yakici <hyakici@k1m13s05.42istanbul.com.tr>
2025-07-16 15:12:44 +0300

    problem no problem no no

--- stack_init.c
+++ stack_init.c
@@ -6,7 +6,7 @@
 /*   By: hyakici <hyakici@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:56:34 by hyakici           #+#    #+#             */
-/*   Updated: 2025/07/16 14:00:14 by hyakici          ###   ########.fr       */
+/*   Updated: 2025/07/16 15:10:25 by hyakici          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ft_stacks_init(t_node **stack_a, t_node **stack_b, char **args)
 	if (!*stack_a)
 	{
 		error_msg_fail(args);
+		exit(EXIT_FAIL);
 		return (0);
 	}
 	free(*args);
